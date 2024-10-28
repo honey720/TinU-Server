186def6a4c496ab1ed7cfad5fcfd8cd8880d8897
honey720 <honey720@naver.com>
2024-10-28 23:30:07 +0900

    [Fix]: Modify BaseEntity.kt(remove "@Column" annotation)

--- src/main/kotlin/com/tinuproject/tinu/domain/entity/base/BaseEntity.kt
+++ src/main/kotlin/com/tinuproject/tinu/domain/entity/base/BaseEntity.kt
@@ -1,11 +1,8 @@
 package com.tinuproject.tinu.domain.entity.base
 
-import jakarta.persistence.Column
 import jakarta.persistence.EntityListeners
 import jakarta.persistence.MappedSuperclass
-import org.springframework.data.annotation.CreatedBy
 import org.springframework.data.annotation.CreatedDate
-import org.springframework.data.annotation.LastModifiedBy
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
@@ -13,11 +10,10 @@ import java.time.LocalDateTime
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
 abstract class BaseEntity {
-    @Column
+
     @CreatedDate
     var createdAt : LocalDateTime? = null
 
-    @Column
     @LastModifiedDate
     var updatedAt : LocalDateTime? = null
 }
