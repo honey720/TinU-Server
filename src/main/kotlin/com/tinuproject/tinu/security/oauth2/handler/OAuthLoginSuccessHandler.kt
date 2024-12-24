6ccdf454ccc1e53be76aca5e3eb4813c8fb6cc82
programofktw <thkim000317@naver.com>
2024-12-24 11:55:07 +0900

    [TINU-83] [Test] httponly attribute Test

--- src/main/kotlin/com/tinuproject/tinu/security/oauth2/handler/OAuthLoginSuccessHandler.kt
+++ src/main/kotlin/com/tinuproject/tinu/security/oauth2/handler/OAuthLoginSuccessHandler.kt
@@ -112,7 +112,6 @@ class OAuthLoginSuccessHandler(
         val encodedName: String = URLEncoder.encode(name, "UTF-8")
         val redirectUri = String.format(REDIRECT_URL, encodedName, accessToken, "done")
 
-
         response?.addHeader("Authorization", accessToken)
         response?.addHeader("Set-Cookie",CookieGenerator.createCookies("AccessToken",accessToken))
         response?.addHeader("Set-Cookie",CookieGenerator.createCookies("RefreshToken", refreshToken))
