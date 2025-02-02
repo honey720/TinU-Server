1133fed24f808ad1116c2c789e6bcf45b1711329
programofktw <thkim000317@naver.com>
2025-02-02 17:45:22 +0900

    [TINU-96] [Fix] response form edit

--- src/main/kotlin/com/tinuproject/tinu/DTO/ResponseDTO.kt
+++ src/main/kotlin/com/tinuproject/tinu/DTO/ResponseDTO.kt
@@ -4,6 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class ResponseDTO(
         @JsonProperty("success") val isSuccess: Boolean,
-        @JsonProperty("state") val stateCode: Int,
+        @JsonProperty("stateCode") val stateCode: Int,
         @JsonProperty("result") val result: Any?
 )
