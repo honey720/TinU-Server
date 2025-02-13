835bede65b3bcd0dbce1c73cf8358c3955c10343
programofktw <thkim000317@naver.com>
2025-02-13 20:32:19 +0900

    [TINU-107] [FIX] NotFound Exception handling

--- src/main/kotlin/com/tinuproject/tinu/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/tinuproject/tinu/GlobalExceptionHandler.kt
@@ -2,10 +2,12 @@ package com.tinuproject.tinu
 
 import com.tinuproject.tinu.DTO.ResponseDTO
 import com.tinuproject.tinu.domain.exception.base.BaseException
+import com.tinuproject.tinu.domain.exception.common.NotFoundException
 import com.tinuproject.tinu.web.ResponseEntityGenerator
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.NoHandlerFoundException
 
 
 @RestControllerAdvice
@@ -14,5 +16,10 @@ class GlobalExceptionHandler {
     @ExceptionHandler(BaseException::class)
     fun baseException(e : BaseException) : ResponseEntity<ResponseDTO>{
         return ResponseEntityGenerator.onFailure(e)
+    }
+
+    @ExceptionHandler(NoHandlerFoundException::class)
+    fun noHandlerFoundException(e : NoHandlerFoundException) : ResponseEntity<ResponseDTO>{
+        return ResponseEntityGenerator.onFailure(NotFoundException())
     }
 }
