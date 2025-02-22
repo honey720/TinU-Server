340fbd8b801cae6336f28d00b8715f57ba73dc93
programofktw <thkim000317@naver.com>
2025-02-22 14:25:13 +0900

    [FIX] RegisterReuqestDTO gender filed delete

--- src/main/kotlin/com/tinuproject/tinu/domain/member/dto/client_controller/RegisterRequestDTO.kt
+++ src/main/kotlin/com/tinuproject/tinu/domain/member/dto/client_controller/RegisterRequestDTO.kt
@@ -5,7 +5,6 @@ data class RegisterRequestDTO(
     val profileImageURL : String?,
     val major : String,
     val grade : Int,
-    val gender : String,
     val eMail : String,
     val introduction : String?
 )
