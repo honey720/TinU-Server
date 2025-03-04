c26a65eeed58a7685b88f680abcf5c0090a8ae6e
honey720 <honey720@kyonggi.ac.kr>
2025-03-04 16:54:13 +0900

    [fix] PostListResponse DTO 구조 변경

--- src/main/kotlin/com/tinuproject/tinu/domain/post/dto/response/PostsListResponseDTO.kt
+++
@@ -1,8 +0,0 @@
-package com.tinuproject.tinu.domain.post.dto.response
-
-
-class PostsListResponseDTO (
-        val posts: List<PostResponseDTO>,
-        val size: Int,
-        val nextCursorId: String
-)
