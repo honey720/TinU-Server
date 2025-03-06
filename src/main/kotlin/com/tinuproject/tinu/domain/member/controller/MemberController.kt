982dd3e574dd97846d77f8ca34f6da6ff61655a9
programofktw <thkim000317@naver.com>
2025-03-06 17:35:53 +0900

    [REFACTOR] swagger annotation remove

--- src/main/kotlin/com/tinuproject/tinu/domain/member/controller/MemberController.kt
+++ src/main/kotlin/com/tinuproject/tinu/domain/member/controller/MemberController.kt
@@ -25,7 +25,6 @@ class MemberController(
     var log : Logger = LoggerFactory.getLogger(this::class.java)
 
     @GetMapping()
-    @SwaggerExceptionResponses(exceptions = [NotExistDomainException::class,ExistMemberException::class])
     fun requestUserInfo(@AuthenticationPrincipal userId : UUID, @RequestParam(name = "userId") searchUserId : String? ) : ResponseEntity<ResponseDTO<MemberSearchResponseDTO?>>{
         val findUserId = searchUserId?.let{UUID.fromString(it)}?:userId
 
