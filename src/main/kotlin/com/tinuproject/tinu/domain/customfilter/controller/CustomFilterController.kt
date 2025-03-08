563070326f34a0d79edc366ca5ca3d116e0ba2a0
programofktw <thkim000317@naver.com>
2025-03-08 15:16:04 +0900

    [TINU-145] [FIX] customFilter-swager-ui-feat

--- src/main/kotlin/com/tinuproject/tinu/domain/customfilter/controller/CustomFilterController.kt
+++ src/main/kotlin/com/tinuproject/tinu/domain/customfilter/controller/CustomFilterController.kt
@@ -4,25 +4,21 @@ import com.tinuproject.tinu.DTO.ResponseDTO
 import com.tinuproject.tinu.domain.customfilter.dto.client_controller.request.CreateCustomFilter
 import com.tinuproject.tinu.domain.customfilter.dto.client_controller.request.DeleteCustomFilter
 import com.tinuproject.tinu.domain.customfilter.dto.client_controller.request.UpdateCustomFilter
+import com.tinuproject.tinu.domain.customfilter.dto.client_controller.response.SelectCustomFilter
 import com.tinuproject.tinu.domain.customfilter.service.CustomFilterService
 import com.tinuproject.tinu.domain.member.service.MemberService
+import com.tinuproject.tinu.web.NullResponse
 import com.tinuproject.tinu.web.ResponseEntityGenerator
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
 
-@Controller
+@RestController
 @RequestMapping("/api/custom-filter")
 class CustomFilterController(
     private val customFilterService: CustomFilterService
@@ -31,18 +27,18 @@ class CustomFilterController(
 
 
     @GetMapping("")
-    fun requestCustomFilter(@AuthenticationPrincipal userId : UUID) : ResponseEntity<ResponseDTO> {
+    fun requestCustomFilter(@AuthenticationPrincipal userId : UUID) : ResponseEntity<ResponseDTO<List<SelectCustomFilter>?>> {
         return ResponseEntityGenerator.onSuccess(customFilterService.getCustomFilter(userId))
     }
 
     @PostMapping("")
-    fun createCustomFilter(@AuthenticationPrincipal userId : UUID, @RequestBody createCustomFilter: CreateCustomFilter) : ResponseEntity<ResponseDTO>{
+    fun createCustomFilter(@AuthenticationPrincipal userId : UUID, @RequestBody createCustomFilter: CreateCustomFilter) : ResponseEntity<ResponseDTO<NullResponse?>>{
         customFilterService.createCustomFilter(userId = userId, createCustomFilter= createCustomFilter)
         return ResponseEntityGenerator.onSuccess()
     }
 
     @PutMapping("/{filterId}")
-    fun updateCustomFilter(@AuthenticationPrincipal userId : UUID, @PathVariable(name = "filterId") filterId : Long,@RequestBody updateCustomFilter: UpdateCustomFilter) : ResponseEntity<ResponseDTO>{
+    fun updateCustomFilter(@AuthenticationPrincipal userId : UUID, @PathVariable(name = "filterId") filterId : Long,@RequestBody updateCustomFilter: UpdateCustomFilter) : ResponseEntity<ResponseDTO<NullResponse?>>{
         updateCustomFilter.filterId = filterId
 
         customFilterService.updateCustomFilter(userId, updateCustomFilter)
@@ -51,7 +47,7 @@ class CustomFilterController(
     }
 
     @DeleteMapping("/{filterId}")
-    fun deleteCustomFilter(@AuthenticationPrincipal userId : UUID, @PathVariable(name = "filterId") filterId: Long) : ResponseEntity<ResponseDTO>{
+    fun deleteCustomFilter(@AuthenticationPrincipal userId : UUID, @PathVariable(name = "filterId") filterId: Long) : ResponseEntity<ResponseDTO<NullResponse?>>{
         customFilterService.deleteCustomFilter(userId, DeleteCustomFilter(filterId=filterId))
 
         return ResponseEntityGenerator.onSuccess()
