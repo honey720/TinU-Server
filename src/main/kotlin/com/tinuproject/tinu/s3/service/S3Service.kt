9f6d84cace5400519b954538a61d15a2f1298726
honey720 <honey720@kyonggi.ac.kr>
2025-03-23 16:45:27 +0900

    [HEAD] [TINU-150] [Fix]: 단일 이미지 검증 로직 구현

--- src/main/kotlin/com/tinuproject/tinu/s3/service/S3Service.kt
+++ src/main/kotlin/com/tinuproject/tinu/s3/service/S3Service.kt
@@ -7,8 +7,10 @@ import com.tinuproject.tinu.s3.dto.response.S3PresignedUrlResponse
 interface S3Service {
     suspend fun getPreSignedUrl(s3PresignedUrlRequest: S3PresignedUrlRequest): S3PresignedUrlResponse
 
-    suspend fun verifyImage(objects: List<S3Verifiable>): List<String>
+    suspend fun verifyImages(objects: List<S3Verifiable>): List<String>
 
-    fun removeImage(objects: List<String>)
+    fun verifyImage(obj: S3Verifiable): String
+
+    fun removeImages(objects: List<String>)
 
 }
