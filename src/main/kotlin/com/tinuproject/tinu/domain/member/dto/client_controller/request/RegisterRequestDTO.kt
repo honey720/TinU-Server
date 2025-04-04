945aadf6c36975b2573546fbf973c346dd619a1f
programofktw <thkim000317@naver.com>
2025-04-04 10:58:59 +0900

    [FEAT} profile image fix move desktop

--- src/main/kotlin/com/tinuproject/tinu/domain/member/dto/client_controller/request/RegisterRequestDTO.kt
+++ src/main/kotlin/com/tinuproject/tinu/domain/member/dto/client_controller/request/RegisterRequestDTO.kt
@@ -1,10 +1,17 @@
 package com.tinuproject.tinu.domain.member.dto.client_controller.request
 
+import com.tinuproject.tinu.s3.dto.S3Verifiable
+
 data class RegisterRequestDTO(
     val nickName : String,
-    val profileImageURL : String?,
+    val profile : Image?,
     val major : String,
     val grade : Int,
     val email : String,
-    val introduction : String?
-)
+    val introduction : String?,
+){
+    data class Image (
+        override val key: String,
+        override val ETag: String
+    ):S3Verifiable
+}
