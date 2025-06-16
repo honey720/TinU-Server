b7f4660c54d4cf91cc7495e0c165a8bdc3ead1bf
programofktw <thkim000317@naver.com>
2025-06-16 15:51:57 +0900

    [FEAT] : Apple Social Enum 추가

--- src/main/kotlin/com/tinuproject/tinu/domain/member/enums/Social.kt
+++ src/main/kotlin/com/tinuproject/tinu/domain/member/enums/Social.kt
@@ -3,14 +3,16 @@ package com.tinuproject.tinu.domain.member.enums
 enum class Social(var code:Int, var company : String) {
     KAKAO(0, "카카오"),
     GOOGLE(1, "구글"),
-    NAVER(2, "네이버");
+    NAVER(2, "네이버"),
+    APPLE(4,"애플");
 
 
     companion object{
         fun getSocial(provider : String) : Social {
             if(provider == "Kakao") return KAKAO
             else if(provider == "Google") return GOOGLE
-            else  return NAVER
+            else if(provider == "Apple") return APPLE
+            else if(provider == "Naver") return NAVER
         }
     }
 
