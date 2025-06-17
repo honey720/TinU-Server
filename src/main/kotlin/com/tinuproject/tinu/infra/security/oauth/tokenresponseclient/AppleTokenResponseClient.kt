d9def61d053f4b2b31be940c91940c8f43f67ae4
programofktw <thkim000317@naver.com>
2025-06-17 15:07:19 +0900

    [FEAT] : Apple Login을 위한 AppleTokenClient feat

---
+++ src/main/kotlin/com/tinuproject/tinu/infra/security/oauth/tokenresponseclient/AppleTokenResponseClient.kt
@@ -0,0 +1,70 @@
+package com.tinuproject.tinu.infra.security.oauth.tokenresponseclient
+
+import com.tinuproject.tinu.global.response.ResponseEntityGenerator
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest
+import org.springframework.security.oauth2.core.OAuth2AccessToken
+import org.springframework.security.oauth2.core.endpoint.OAuth2AccessTokenResponse
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.exchange
+
+
+class AppleTokenResponseClient(
+    private val jwtGenerator: () -> String
+) : OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest>{
+
+    private val restTemplate = RestTemplate()
+
+    val log : Logger = LoggerFactory.getLogger(this::class.java)
+    override fun getTokenResponse(request: OAuth2AuthorizationCodeGrantRequest): OAuth2AccessTokenResponse {
+        val clientRegistration = request.clientRegistration
+        val redirectUri = request.authorizationExchange.authorizationRequest.redirectUri
+        val code = request.authorizationExchange.authorizationResponse.code
+
+        val jwtToken = jwtGenerator()
+
+        val formData = LinkedMultiValueMap<String, String>().apply {
+            add("client_id", clientRegistration.clientId)
+            add("client_secret",jwtToken)
+            add("code",code)
+            add("grant_type", "authorization_code")
+            add("redirect_uri", redirectUri)
+        }
+
+
+        val headers = HttpHeaders().apply {
+            contentType =  MediaType.APPLICATION_FORM_URLENCODED
+        }
+
+        val entity = HttpEntity(formData, headers)
+
+
+        val response = restTemplate.exchange(
+            clientRegistration.providerDetails.tokenUri,
+            HttpMethod.POST,
+            entity,
+            object : ParameterizedTypeReference<Map<String, Any>>(){}
+        ).body ?: throw Exception("소셜 로그인 실패 Exception")
+
+        val accessToken = response["access_token"] as String
+        val refreshToken = response["refresh_token"] as? String
+        val expiresIn = (response["expires_in"]as Number).toLong()
+
+
+        return OAuth2AccessTokenResponse.withToken(accessToken)
+            .tokenType(OAuth2AccessToken.TokenType.BEARER)
+            .expiresIn(expiresIn)
+            .refreshToken(refreshToken)
+            .scopes(clientRegistration.scopes)
+            .additionalParameters(response)
+            .build()
+    }
+}
