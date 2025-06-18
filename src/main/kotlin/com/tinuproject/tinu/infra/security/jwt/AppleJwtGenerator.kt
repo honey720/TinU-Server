0c69584337a1815fcfa32ff0b526b49cb1732da8
programofktw <thkim000317@naver.com>
2025-06-18 12:43:19 +0900

    [FEAT] : code를 통한 Token 호출 시 clientSecret으로 보낼 Jwt Token 생성 class 인 ApplejwtGenerator 클래스 생성

---
+++ src/main/kotlin/com/tinuproject/tinu/infra/security/jwt/AppleJwtGenerator.kt
@@ -0,0 +1,46 @@
+package com.tinuproject.tinu.infra.security.jwt
+
+import com.tinuproject.tinu.infra.security.config.AppleProperties
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.stereotype.Component
+import java.security.KeyFactory
+import java.security.PrivateKey
+import java.security.spec.PKCS8EncodedKeySpec
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+import java.util.*
+import java.util.Date.*
+
+@Component
+class AppleJwtGenerator(
+    val appleProperties: AppleProperties
+) {
+    fun generate(): String{
+        val now = Instant.now()
+
+        val exp = now.plus(180,ChronoUnit.DAYS)
+
+        return Jwts.builder()
+            .setHeaderParam("kid", appleProperties.serviceKey)
+            .setIssuer(appleProperties.teamId)
+            .setIssuedAt(from(now))
+            .setExpiration(from(exp))
+            .setAudience("https://appleid.apple.com")
+            .setSubject(appleProperties.clientId)
+            .signWith(loadPrivateKey(), SignatureAlgorithm.ES256)
+            .compact()
+    }
+
+    private fun loadPrivateKey() : PrivateKey {
+        val privateKeyPem = appleProperties.clientSecret
+            .replace("-----BEGIN PRIVATE KEY-----","")
+            .replace("-----END PRIVATE KEY-----","")
+            .replace("\\s+".toRegex(),"")
+
+        val keyBytes = Base64.getDecoder().decode(privateKeyPem)
+        val keySpec = PKCS8EncodedKeySpec(keyBytes)
+
+        return KeyFactory.getInstance("EC").generatePrivate(keySpec)
+    }
+}
