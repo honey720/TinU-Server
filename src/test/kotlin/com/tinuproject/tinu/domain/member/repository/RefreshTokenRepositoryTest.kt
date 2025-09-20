8e9f9d2aaa6ccdc06dff16fe8c02d9cb929dc183
programofktw <thkim000317@naver.com>
2025-09-20 15:29:32 +0900

    [REFACTOR] JpaAuditing 설정 별도 config 파일로 추출

---
+++ src/test/kotlin/com/tinuproject/tinu/domain/member/repository/RefreshTokenRepositoryTest.kt
@@ -0,0 +1,31 @@
+package com.tinuproject.tinu.domain.member.repository
+
+import com.tinuproject.tinu.domain.member.entity.RefreshToken
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.ActiveProfiles
+import java.util.*
+
+@DataJpaTest
+@ActiveProfiles("test")
+class RefreshTokenRepositoryTest {
+
+    @Autowired
+    private lateinit var refreshTokenRepository : RefreshTokenRepository
+
+    @Test
+    @DisplayName("123123")
+    fun Test(){
+        //Given
+        val userId =  UUID.randomUUID();
+        val token = UUID.randomUUID().toString();
+        val refreshToken = RefreshToken(userId = userId, token = token)
+
+        refreshTokenRepository.save(refreshToken);
+        //When
+
+        //Then
+    }
+}
