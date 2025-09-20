a1da9c5ae156005e1db3ffe57ff6f59e74f1d458
programofktw <thkim000317@naver.com>
2025-09-20 16:58:54 +0900

    [FEAT] 테스트 커스텀 어노테이션 추가

---
+++ src/test/kotlin/com/tinuproject/tinu/annotation/MockControllerTest.kt
@@ -0,0 +1,16 @@
+package com.tinuproject.tinu.annotation
+
+import com.tinuproject.tinu.domain.member.controller.CustomFilterController
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.core.annotation.AliasFor
+import org.springframework.test.context.ActiveProfiles
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@WebMvcTest
+@ActiveProfiles("test")
+annotation class  MockControllerTest (
+    @get:AliasFor(annotation = WebMvcTest::class, attribute = "controllers")
+    val controllers: Array<KClass<*>> = []
+)
