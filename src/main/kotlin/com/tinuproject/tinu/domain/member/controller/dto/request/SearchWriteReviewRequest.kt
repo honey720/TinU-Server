984ff36463470992c401de267e55d0271748ca1b
programofktw <thkim000317@naver.com>
2025-10-12 20:25:45 +0900

    [FEAT] Review 관련 API Swagger 설명 추가

--- src/main/kotlin/com/tinuproject/tinu/domain/member/controller/dto/request/SearchWriteReviewRequest.kt
+++ src/main/kotlin/com/tinuproject/tinu/domain/member/controller/dto/request/SearchWriteReviewRequest.kt
@@ -1,15 +1,17 @@
 package com.tinuproject.tinu.domain.member.controller.dto.request
 
 import com.tinuproject.tinu.domain.member.service.dto.input.SearchWriteReviewInput
+import io.swagger.v3.oas.annotations.media.Schema
 import java.util.*
 
+@Schema(description = "리뷰 작성 필요 여부 확인")
 class SearchWriteReviewRequest(
-
+    @Schema(description = "거래 완료된 채팅방의 post Id")
     val postId : Long
 ) {
-    fun of(reviewrId : UUID) : SearchWriteReviewInput{
+    fun of(reviewerId : UUID) : SearchWriteReviewInput{
         return SearchWriteReviewInput(
-            userId = reviewrId,
+            userId = reviewerId,
             postId = this.postId
         )
     }
