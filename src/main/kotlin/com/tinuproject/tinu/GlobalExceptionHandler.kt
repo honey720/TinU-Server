b1ae97eacbf2aac42e1fe7d0730fe91d7b32c487
programofktw <thkim000317@naver.com>
2025-10-17 12:16:39 +0900

    [FEAT] 요청 파라미터 값 타입 오류시 처리하는 GlobalExceptionHandler 추가

--- src/main/kotlin/com/tinuproject/tinu/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/tinuproject/tinu/GlobalExceptionHandler.kt
@@ -1,11 +1,13 @@
 package com.tinuproject.tinu
 
+import com.tinuproject.tinu.global.exception.BadRequestException
 import com.tinuproject.tinu.global.response.dto.ErrorResponse
 import com.tinuproject.tinu.global.response.dto.ResponseDTO
 import com.tinuproject.tinu.global.exception.base.BaseException
 import com.tinuproject.tinu.global.exception.NotFoundException
 import com.tinuproject.tinu.global.response.ResponseEntityGenerator
 import org.springframework.http.ResponseEntity
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.servlet.NoHandlerFoundException
@@ -22,5 +24,10 @@ class GlobalExceptionHandler {
     @ExceptionHandler(NoHandlerFoundException::class)
     fun noHandlerFoundException(e : NoHandlerFoundException) : ResponseEntity<ResponseDTO<ErrorResponse>>{
         return ResponseEntityGenerator.onFailure(NotFoundException())
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException::class)
+    fun argumentTypeMissMatchException(e :MethodArgumentTypeMismatchException) : ResponseEntity<ResponseDTO<ErrorResponse>>{
+        return ResponseEntityGenerator.onFailure(BadRequestException())
     }
 }
